3b21c44fe1ab174f67a0e10edf14204684e24daa
Chinmay Pendharkar <notthetup@gmail.com>
2014-01-19 15:55:12 +0800

    reorg

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,60 @@
-
 module.exports = function(grunt) {
-grunt.initConfig({
-	connect: {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		dirs: {
+			src: 'js',
+			dest: '.'
+		},
+		jshint: {
+			all: ['Gruntfile.js', '<%= dirs.src %>/*.js'],
+		},
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+				preserveComments: false,
+				compress: true,
+				mangle: true
+			},
+			build: {
+				src: '<%= dirs.src %>/<%= pkg.name %>.js',
+				dest: '<%= dirs.dest %>/<%= pkg.name %>.min.js'
+			},
+		},
+		watch: {
+			scripts: {
+				files: ['<%= dirs.src %>/*.js',],
+				tasks: ['jshint'],
+				options: {
+					spawn: false,
+				},
+			},
+		},
+		connect: {
 			server: {
 				options: {
 					port: 8080,
-					base: '',
-					keepalive: 'true'
+					base: ''
 				}
+			}
+		},
+		browserify: {
+			basic: {
+				src: 'app.js',
+				dest: 'bundle.js'
 			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-browserify');
 
-}
+	grunt.registerTask('default', ['jshint','uglify','browserify']);
+
+	// Serve presentation locally
+	grunt.registerTask( 'serve', ['jshint','uglify','browserify', 'connect', 'watch' ] );
+
+};
