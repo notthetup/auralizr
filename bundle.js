2bfa31f3e7b4353f8ed49352d5f9a06c1e7e8e5a
Sayanee <sayanee@gmail.com>
2014-01-19 21:29:17 +0800

    moved back to dropbox + fixed classname

--- bundle.js
+++ bundle.js
@@ -3,25 +3,32 @@
 	var aur = require('./js/auralizr.js');
 	var auralizr = new aur();
 
-	var impulseResponses = {
+	/*var impulseResponses = {
 		'mausoleum' : 'http://notthetup.github.io/auralizr/audio/h.wav',
 		'basement' : 'http://notthetup.github.io/auralizr/audio/s1.wav',
 		'chapel' : 'http://notthetup.github.io/auralizr/audio/sb.wav',
 		'stairwell' : 'http://notthetup.github.io/auralizr/audio/st.wav'
+	}*/
+
+	var impulseResponses = {
+		'mausoleum' : 'https://dl.dropboxusercontent.com/u/957/IRs/converted/h.wav',
+		'basement' : 'https://dl.dropboxusercontent.com/u/957/IRs/converted/s1.wav',
+		'chapel' : 'https://dl.dropboxusercontent.com/u/957/IRs/converted/sb.wav',
+		'stairwell' : 'https://dl.dropboxusercontent.com/u/957/IRs/converted/st.wav'
 	}
 
 	var allSpans = [].slice.call(document.getElementsByTagName('span'));
 	allSpans.forEach(function(element) {
 		element.addEventListener('click',function(event){
 			auralizr.stop();
-			auralizr.use(this.class);
+			auralizr.use(this.className);
 			auralizr.start();
 		}, false)
 	})
 
 	for( var key in impulseResponses){
 		auralizr.load(impulseResponses[key], key, function (key){
-			var span = document.getElementsByClass(key)[0];
+			var span = document.getElementsByClassName(key)[0];
 			if (span) console.log('enable span ' + key);
 		});
 	}
