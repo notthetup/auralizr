d56dba4d407500c817f307cf3baebdfd926b9cf6
Sayanee <sayanee@gmail.com>
2014-01-19 18:38:36 +0800

    sass compass integrated with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,18 @@
 module.exports = function(grunt) {
 
 	grunt.initConfig({
+
+		compass: {
+			production: {
+				options: {
+					specify: 'sass/style.sass',
+					cssPath: '.',
+					outputStyle: 'compressed',
+					noLineComments: true
+				}
+			}
+		},
+
 		connect: {
 			server: {
 				options: {
@@ -9,11 +21,27 @@ module.exports = function(grunt) {
 					keepalive: 'true'
 				}
 			}
+		},
+
+		watch: {
+
+			css: {
+				files: ['**/*.sass'],
+				tasks: ['compass'],
+				options: {
+					spawn: false,
+				}
+			}
+
 		}
+
 	});
 
+	grunt.loadNpmTasks('grunt-contrib-compass');
 	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('default', ['connect']);
+	grunt.registerTask('default', ['compass', 'watch']);
+	grunt.registerTask('serve', ['connect']);
 
 }
--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-auralizejs
-==========
+#Auralizr
 
 Simple Auralization Test using the WebAudioAPI
+
+#Credit
+
+The 4 photos were from Wikipedia Commons:
+
+1. [Stairwell photo](http://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Monument_stairwell.JPG/1280px-Monument_stairwell.JPG)
+1. [Chapel photo](http://en.wikipedia.org/wiki/File:138_CHURCH_07_12_11.jpg)
+1. [Mausoleum](http://upload.wikimedia.org/wikipedia/commons/5/5c/Avicenna_Mausoleum_interior.jpg)
+1. [Basement](http://en.wikipedia.org/wiki/File:Sklep_cejkovice.JPG)
+
+
+
