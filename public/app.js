f6bfedeb6012621a9b5fda726aef839cde20a8e6
Chinmay Pendharkar <notthetup@gmail.com>
2021-10-03 15:39:27 +0800

    updated everything

---
+++ public/app.js
@@ -0,0 +1,42 @@
+import { Auralizr } from "./auralizr.js";
+
+var aur = new Auralizr();
+var impulseResponses = [
+	{id: 'mausoleum', path: 'audio/h.wav', buffer: []},
+	{id: 'basement', path: 'audio/s1.wav', buffer: []},
+	{id: 'chapel', path: 'audio/sb.wav', buffer: []},
+	{id: 'stairwell', path: 'audio/st.wav', buffer: []}
+];
+
+document.addEventListener('DOMContentLoaded', async () => {
+	impulseResponses.forEach( async ir => {
+		await loadIR(ir);
+		let btn = document.getElementById(ir.id);
+		btn.addEventListener('click', evt => {
+			let el = evt.target
+			if (el.innerHTML === '▶'){
+				resetAll();
+				aur.setIR(ir.buffer);
+				if (!aur.isRunning) aur.start();
+				el.classList.add('enabled');
+				el.innerHTML = '❚❚';
+			}else{
+				aur.stop();
+				resetAll();
+			}
+		});
+		btn.innerHTML = '▶';
+	});
+});
+
+function resetAll() {
+	[].slice.call(document.getElementsByClassName('place')).forEach( el => {
+		el.classList.remove('enabled');
+		if (el.innerHTML === '❚❚')
+		el.innerHTML = '▶';
+	});
+}
+
+async function loadIR(ir){
+	ir.buffer = await fetch(ir.path).then(res => res.arrayBuffer()).then(buf => aur.audioContext.decodeAudioData(buf))
+}
---
+++ public/auralizr.js
@@ -0,0 +1,1 @@
+../auralizr.js
